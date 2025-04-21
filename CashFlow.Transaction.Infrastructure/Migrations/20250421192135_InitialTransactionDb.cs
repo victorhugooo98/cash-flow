673db2c2001de4a75b1360119f7611a14fe3128b
Victor Hugo <victor.schmidt@wasolutions.co>
2025-04-21 16:31:03 -0300

    Adding migrations

---
+++ CashFlow.Transaction.Infrastructure/Migrations/20250421192135_InitialTransactionDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashFlow.Transaction.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTransactionDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MerchantId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_MerchantId_Timestamp",
+                table: "Transactions",
+                columns: new[] { "MerchantId", "Timestamp" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
